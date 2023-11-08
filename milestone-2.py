8a077be5a96f13c4426e150adc3f306377b1dbde
Michael Daley <mich@Michaels-MacBook-Air-3.local>
2023-11-08 00:20:24 -0500

    cleaned up functions to use premade dirs

--- milestone-2.py
+++ milestone-2.py
@@ -1,10 +1,14 @@
-import uvicorn
 import whisper
 import os
-from fastapi import FastAPI, HTTPException
+import ffmpeg
+from flask import Flask
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
+
+'''
+    to run api paste " uvicorn milestone-2:app " in terminal
+'''
 
 def download_audio(url:str, download_path:str):
 
@@ -16,11 +20,11 @@ def download_audio(url:str, download_path:str):
         audio.download(output_path=download_path, filename=file_name)
     
     except KeyError:
-        raise HTTPException(status_code=400, detail="Error: audio souce not avaliable or cannot be download")
+        return 400, "Error: audio souce not avaliable or cannot be download"
     except ValueError:
-        raise HTTPException(status_code=400, detail="Error: invalide URL")
+        return 400, "Error: invalide URL"
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Error downloading video: " + str(e))
+        return 400, "Error downloading video: " + str(e)
     
     return os.path.join(download_path, file_name)
     
@@ -38,12 +42,29 @@ def download_captions(url:str, download_path:str):
         with open(file_path, 'w', encoding='utf-8') as srt_file:
             srt_file.write(srt_formatted)
     except KeyError:
-        raise HTTPException(status_code=400, detail="Error: video not avaliable or cannot be download")
+        return 400, "Error: video not avaliable or cannot be download"
     except ValueError:
-        raise HTTPException(status_code=400, detail="Error: invalide URL")
+        return 400, "Error: invalide URL"
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Error extracting transcript from: " + str(e))
+        400, "Error extracting transcript from: " + str(e)
+
+def sep_audio(video:str, save_path): #seperates audio from video file
+    try:
+        input = ffmpeg.input(video)
+        audio = input.audio.filter("anull")
+    except FileNotFoundError:
+        print("%s file couldn't be accessed"%video)
     
+    temp = video.split('/')[-1] #gets last element if a file path
+    file_name = temp.split('.')[0] + '.mp3'
+    file_path = os.path.join(save_path, file_name)
+    try:
+        output = ffmpeg.output(audio, file_path)
+        output.run()
+        return file_path
+    except:
+        print("error creating audio file")
+
 
 def transcribe_audio(input_file:str, output_path:str): #eventually add a check for if file is mp3
     try:
@@ -63,45 +84,33 @@ def transcribe_audio(input_file:str, output_path:str): #eventually add a check f
         print("%s this dir can't be accessed ", output_path)
         
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.get("/download")
 
-async def main():
+@app.route("/")
+
+def main():
     audio_dir_name = 'audio'
-    cap_dir_name = 'captions'
+    video_dir_name = 'videos'
     whisper_dir_name = 'whisper_transcripts'
     current_dir = os.getcwd()
     audio_dir_path = os.path.join(current_dir, audio_dir_name)
-    cap_dir_path = os.path.join(current_dir, cap_dir_name)
+    video_dir_path = os.path.join(current_dir, video_dir_name)
     whisper_dir_path = os.path.join(current_dir, whisper_dir_name)
 
     if(not(os.path.exists(audio_dir_path))):
         os.makedirs(audio_dir_path)
-    if(not(os.path.exists(cap_dir_path))):
-        os.makedirs(cap_dir_path)
     if(not(os.path.exists(whisper_dir_path))):
         os.makedirs(whisper_dir_path)
-
-    youtube_links = ['https://www.youtube.com/watch?v=bNKdlnoAqIs&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=27',
-                    'https://www.youtube.com/watch?v=RldpC8a9Zv4&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=31',
-                    'https://www.youtube.com/watch?v=aDzm9_vthFo&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=32',
-                    'https://www.youtube.com/watch?v=6in8fx1Tc38&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=37',
-                    'https://www.youtube.com/watch?v=n9adLsXTpZQ&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=63',
-                    'https://www.youtube.com/watch?v=_Lx5VmAdZSI&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=54',
-                    'https://www.youtube.com/watch?v=EIVTf-C6oQo&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=58',
-                    'https://www.youtube.com/watch?v=FwEKCP7DVFc&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=56',
-                    'https://www.youtube.com/watch?v=cU3rmlDgfbg&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=60',
-                    'https://www.youtube.com/watch?v=Du-RQu4soIs&list=PLI1yx5Z0Lrv77D_g1tvF9u3FVqnrNbCRL&index=61'
-                    ]
     
-    for i in range(len(youtube_links)):
-        current_link = youtube_links[i]
-        download_captions(current_link, cap_dir_path)
-        download_audio(current_link, audio_dir_path)
 
-    files = os.listdir(audio_dir_path) # accessing all files in audio dir
-    in_file = os.path.join(audio_dir_path, files[0]) #grabs first file in the directory 
+    files = os.listdir(video_dir_path) # accessing all files in audio dir
+    in_file = os.path.join(video_dir_path, files[0]) #grabs first file in the directory 
 
-    transcribe_audio(in_file,whisper_dir_path) 
 
+    transcribe_audio(sep_audio(in_file,audio_dir_path),whisper_dir_path) 
+
+    return 200, "audio has been transcribed successfully"
+
+if __name__ == "__main__":
+    app.run(debug=True)
