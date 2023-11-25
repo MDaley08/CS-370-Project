6fde10e88a9d7a5814f70ccff755a0f83fb1a748
Michael Daley <mdaley08@outlook.com>
2023-11-25 02:16:56 -0500

    rolled milestone 3 into main

--- foreign_whispers.py
+++ foreign_whispers.py
@@ -1,10 +1,13 @@
 import whisper
 import os
 import ffmpeg
+import textwrap 
 from flask import Flask
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import SRTFormatter
+from deep_translator import GoogleTranslator
+
 
 '''
     to run api paste " uvicorn milestone-2:app " in terminal
@@ -30,6 +33,7 @@ def download_audio(url:str, download_path:str):
     
 
 def download_captions(url:str, download_path:str):
+
     formatter = SRTFormatter()
 
     try:
@@ -48,7 +52,10 @@ def download_captions(url:str, download_path:str):
     except Exception as e:
         400, "Error extracting transcript from: " + str(e)
 
-def sep_audio(video:str, save_path): #seperates audio from video file
+    srt_file.close()
+
+def sep_audio(video:str, output_path): #seperates audio from video file
+
     try:
         input = ffmpeg.input(video)
         audio = input.audio.filter("anull")
@@ -57,7 +64,8 @@ def sep_audio(video:str, save_path): #seperates audio from video file
     
     temp = video.split('/')[-1] #gets last element if a file path
     file_name = temp.split('.')[0] + '.mp3'
-    file_path = os.path.join(save_path, file_name)
+    file_path = os.path.join(output_path, file_name)
+
     try:
         output = ffmpeg.output(audio, file_path)
         output.run()
@@ -71,17 +79,46 @@ def transcribe_audio(input_file:str, output_path:str): #eventually add a check f
         model = whisper.load_model("base")
         result = model.transcribe(input_file)
     except FileNotFoundError:
-        print("%s file was not found ", input_file)
+        print("%s file was not found " % input_file)
 
     try:
         file_name = input_file.split('/')[-1]
         file_name = file_name.split('.')[0]
-        file_path = os.path.join(output_path, file_name)
+        file_path = os.path.join(output_path, file_name) + ".txt"
         with open(file_path, 'w', encoding='utf-8') as out_file:
-            out_file.write(result["text"])
+            wrapped_text = textwrap.fill(result["text"], width=100)
+            out_file.write(wrapped_text)
 
     except FileNotFoundError:
-        print("%s this dir can't be accessed ", output_path)
+        print("%s this dir can't be accessed " % output_path)
+    
+    out_file.close()
+    return(file_path)
+
+def translate_text(input_file:str, output_path:str, lang: str):
+
+    translator = GoogleTranslator(source= 'english', target=lang)
+
+    try: #try to open our caption file
+        in_file = open(input_file, 'r', encoding="utf8") #opening file to read
+    except FileNotFoundError:
+        print("%s file was not found " % input_file)
+
+    try: #try to create a new file to store translation
+        out_file_name = (input_file.split('/')[-1]).split('.')[0] + ' translation.txt' # we do a split incase file is abs path then take old name
+        out_file_path = os.path.join(output_path, out_file_name)
+        out_file = open(out_file_path, 'w', encoding='utf8')
+    except FileNotFoundError:
+        print("%s this dir can't be accessed " % output_path)
+        
+    for i in in_file.readlines(): #reading all files in the 'captions' directory
+        translated_line = translator.translate(i)
+        out_file.write(translated_line+'\n')
+
+    print('%s has be sucessfully translate' % input_file)
+    in_file.close()
+    out_file.close()
+
         
 
 app = Flask(__name__)
@@ -108,7 +145,7 @@ def main():
     in_file = os.path.join(video_dir_path, files[0]) #grabs first file in the directory 
 
 
-    transcribe_audio(sep_audio(in_file,audio_dir_path),whisper_dir_path) 
+    translate_text(transcribe_audio(sep_audio(in_file,audio_dir_path),whisper_dir_path),current_dir,'german')
 
     return 200, "audio has been transcribed successfully"
 
