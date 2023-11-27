69bf17490e22f209b388e9abe27a88db0f336fb7
Michael Daley <mdaley08@outlook.com>
2023-11-26 23:25:46 -0500

    completed text-to-speech function

--- milestone-4.py
+++ milestone-4.py
@@ -1,9 +1,26 @@
 import os
 from gtts import gTTS
+import gtts.lang
 
 
 def text_to_speech(input_file:str, output_path:str):
-    pass
+    try:
+        in_file = open(input_file,'r',encoding="utf8")
+
+    except FileNotFoundError:
+        print("%s file was not found \n" % input_file)
+    
+    out_file_name = (input_file.split('/')[-1]).split('.')[0] + '.mp3'
+    out_file_path = os.path.join(output_path, out_file_name)
+
+    in_text = ""
+    language = 'de'
+
+    for i in in_file.readlines():
+        in_text += i
+
+    output = gTTS(text= in_text, lang= language)
+    output.save(out_file_path)
 
 def main():
 
@@ -14,13 +31,12 @@ def main():
     trs_dir_path = os.path.abspath(trs_dir_name)
     tts_dir_path = os.path.join(current_dir, tts_dir_name)
 
+    if(not(os.path.exists(tts_dir_path))):
+        os.makedirs(tts_dir_path)
 
-    # if(not(os.path.exists(trs_dir_path))):
-    #     os.makedirs(trs_dir_path)
-
-    # for file in os.listdir(text_dir_path): #iterates through each caption file
-    #     file_path = os.path.join(text_dir_path,file)
-    #     translate_text(file_path,trs_dir_path,'german')
+    for file in os.listdir(trs_dir_path): #iterates through each translation file
+        file_path = os.path.join(trs_dir_path, file)
+        text_to_speech(file_path,tts_dir_path)
 
 
 main()
